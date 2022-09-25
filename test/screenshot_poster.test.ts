ae1637dd26710c36154d36258a9d8f9c2c0e593c
Kamil Hismatullin <kamil.hismatullin@flatstack.com>
2022-09-25 14:54:18 +0200

    Initial commit

---
+++ test/screenshot_poster.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ScreenshotPoster from '../lib/screenshot_poster-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/screenshot_poster-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ScreenshotPoster.ScreenshotPosterStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
